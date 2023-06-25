f38abbf4f9626d1dd83d73a1f25d0522920b9dd4
DinghaoZou <dinghaozou0@gmail.com>
2023-06-24 22:38:45 -0700

    a

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public int difficulty;
-    
+
+    public int number1;
+    public int number2;
+
+    public int theScore;
+    public int highScore;
+
+    public InputField answerInput;
+    public Text theTextOfTheEquation;
+
+    public int NumberThatWillDecideTheEquation;
+    public string theEquationSymbol;
 
     // Start is called before the first frame update
     void Start()
@@ -17,5 +29,28 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void UpdateEquation () {
+        if (difficulty == 0) {
+            NumberThatWillDecideTheEquation = 0;
+        } else if (difficulty == 1) {
+            NumberThatWillDecideTheEquation = Random.Range(0, 1);
+        } else if (difficulty == 2) {
+            NumberThatWillDecideTheEquation = Random.Range(0, 2);
+        }
+
+        if (NumberThatWillDecideTheEquation == 0) {
+            theEquationSymbol = "+";
+        } else if (NumberThatWillDecideTheEquation == 1) {
+            theEquationSymbol = "-";    
+        } else if (NumberThatWillDecideTheEquation == 2) {
+            theEquationSymbol = "x";
+        }
+
+        number1 = Random.Range(0, 9);
+        number2 = Random.Range(0, 9);
+
+
     }
 }
