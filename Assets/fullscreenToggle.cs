1a89dbdd8f1b17f2bc8b72f5f40916217a10f011
DinghaoZou <dinghaozou0@gmail.com>
2023-07-15 22:42:28 -0700

    a

--- Assets/fullscreenToggle.cs
+++ Assets/fullscreenToggle.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class fullscreenToggle : MonoBehaviour
-{
+{   
+    public string pesudoBoolFullscreen = "true";
     public Text fullscreenButtonSwitch;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        PlayerPrefs.GetString("Fullscreen");
+
+        if (pesudoBoolFullscreen == "true") {
+            Screen.fullScreen = true;
+            fullscreenButtonSwitch.text = "On";
+        } else if (pesudoBoolFullscreen == "false") {
+            Screen.fullScreen = false;
+            fullscreenButtonSwitch.text = "Off";
+        }
     }
 
     // Update is called once per frame
@@ -25,5 +34,13 @@ public class fullscreenToggle : MonoBehaviour
 
     public void toggleFullscreen () {
         Screen.fullScreen = !Screen.fullScreen;
+        
+        if (!Screen.fullScreen) {
+            pesudoBoolFullscreen = "false";
+        } else if (Screen.fullScreen) {
+            pesudoBoolFullscreen = "true";
+        }
+
+        PlayerPrefs.SetString("Fullscreen", pesudoBoolFullscreen);
     }
 }
